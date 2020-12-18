103b5b47106ae03d8a076092b4b218c87ee11aba
Joseph Baines <50284158+Joseph-Baines@users.noreply.github.com>
2020-12-18 14:53:20 -0500

    Update KMSCalculator.py

--- KMSCalculator.py
+++ KMSCalculator.py
@@ -4,4 +4,4 @@ miles = float(kms)/1.60934
 miles = round(miles, 2)
 print(f"Ok, your {kms}km ride was {round(miles, 2)} miles.")
 
-#round(thing to round, how many decimal points)
+#round(thing to round, how many decimal points) 
